c721baa6e0bacafb7ffe0cc9c8d405fba6c9417b
ConCorde0 <1462913972@qq.com>
2025-09-27 18:25:49 +0800

    feat:基础角色移动以及物理碰撞处理

--- Assets/a_Scripts/CharacterAuthoring.cs
+++ Assets/a_Scripts/CharacterAuthoring.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+public struct InitializedCharacterFlag : IComponentData, IEnableableComponent
+{
+    
+}
 public struct CharacterMoveDirection : IComponentData
 {
     public float2 Value;
@@ -16,7 +23,6 @@ public struct CharacterMoveSpeed : IComponentData
 }
 
 
-
 public class CharacterAuthoring : MonoBehaviour
 {
    public float moveSpeed;
@@ -26,11 +32,40 @@ public class CharacterAuthoring : MonoBehaviour
         public override void Bake(CharacterAuthoring authoring)
         {
             var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<InitializedCharacterFlag>(entity);
             AddComponent<CharacterMoveDirection>(entity);
             AddComponent(entity, new CharacterMoveSpeed
             {
                 Value = authoring.moveSpeed
             });
+        }
+    }
+}
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public partial struct CharacterInitializationSystem : ISystem
+{
+    [BurstCompile]  
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (mass, shouldInitialize) in SystemAPI.Query<RefRW<PhysicsMass>, EnabledRefRW<InitializedCharacterFlag>>())
+        {
+            mass.ValueRW.InverseInertia = float3.zero;
+            shouldInitialize.ValueRW = false;
+        }
+    }
+}
+
+public partial struct CharacterMoveSystem : ISystem
+{
+    [BurstCompile]  
+    public void OnUpdate(ref SystemState state)
+    {
+        var deltaTime = SystemAPI.Time.DeltaTime;
+        foreach (var (velocity, direction, speed) in SystemAPI.Query<RefRW<PhysicsVelocity>, RefRO<CharacterMoveDirection>, RefRO<CharacterMoveSpeed>>())
+        {
+            var moveStep2d = direction.ValueRO.Value * speed.ValueRO.Value;
+            velocity.ValueRW.Linear = new float3(moveStep2d, 0);
         }
     }
 }
